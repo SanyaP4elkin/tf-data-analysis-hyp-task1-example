6d70bf9fc627ce84a1e4c149b15388b3a71868c2
SanyaP4elkin <104874172+SanyaP4elkin@users.noreply.github.com>
2023-04-12 22:54:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,10 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    prop = proportions_chisquare([x_success, x_cnt], 
+                                 [y_success, y_cnt])
+    p = prop[1]
+    if (p > 0.08) and (x_success/x_cnt < y_success/y_cnt):
+        return True
+    else:
+        return False
