03a58a9d1a74d9ea8ffee682d8fb477c852cf163
SanyaP4elkin <104874172+SanyaP4elkin@users.noreply.github.com>
2023-04-13 15:02:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.proportion import proportions_chisquare
+from scipy.stats import mannwhitneyu
 
 chat_id = 1126746074 # Ваш chat ID, не меняйте название переменной
 
@@ -9,8 +9,8 @@ def solution(x_success: int,
              y_success: int, 
              y_cnt: int) -> bool:
     p_ar = []
-    p1 = len(x_success)/len(x_cnt)
-    p2 = len(y_success)/len(y_cnt)
+    p1 = x_success/x_cnt
+    p2 = y_success/y_cnt
     for i in range(1000):
         n1 = np.random.binomial(1, p1, size = 5000)
         n2 = np.random.binomial(1, p2, size = 5000)
