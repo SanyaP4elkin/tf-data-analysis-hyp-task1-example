d0e06a14cbb3e9c3f0227fa89e7f15f11c97a24f
SanyaP4elkin <104874172+SanyaP4elkin@users.noreply.github.com>
2023-04-13 10:40:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,10 +8,18 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    prop = proportions_chisquare([x_success, x_cnt], 
-                                 [y_success, y_cnt])
-    p = prop[1]
-    if (p > 0.08) and (x_success/x_cnt < y_success/y_cnt):
+    p_ar = []
+    p1 = len(x_success)/len(x_cnt)
+    p2 = len(y_success)/len(y_cnt)
+    for i in range(1000):
+        n1 = np.random.binomial(1, p1, size = 5000)
+        n2 = np.random.binomial(1, p2, size = 5000)
+        p_ar.append(mannwhitneyu(n1, n2, alternative = 'greater').pvalue)
+        break
+    p_ar = np.array(p_ar)
+    
+    if (np.mean(p_ar > 0.92) > 0.92) and (p2 > p1):
         return True
     else:
         return False
+        
